57b984dfbd3c41c33b1ce6a8fb4c1f30df74e4bb
QICHAO HONG <hqc@Erwins-MacBook-Pro.local>
2020-02-27 04:22:22 -0500

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
     url: "data2free", // the auth0 domain prefix
     audience: "casting", // the audience set for the auth0 app
     clientId: "9VYAbSI2oMKlH3430Y52JE66t2Wa8BwL", // the client id generated for the auth0 app
-    callbackURL: "https://casting-agency-backend.herokuapp.com/" // the base url of the running ionic application.
+    callbackURL: "https://casting-agency-frontend.herokuapp.com/" // the base url of the running ionic application.
   }
 };
